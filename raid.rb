c71a6cef47c892710e12d5b235c3da9fc753601d
jemiam <jemiam@gmail.com>
2010-07-10 21:34:47 +0900

    tmp/stop.txtがあったら出撃中止

--- raid.rb
+++ raid.rb
@@ -1,4 +1,8 @@
 require 'dracru2'
 
+if File.exists?(File.join(TMP_PATH, 'stop.txt'))
+  $logger.info("Raid was stopped. Remove tmp/stop.txt if you want to raid again.")
+  exit
+end
 dracru2 = Dracru2.new
 dracru2.raid_if_possible
