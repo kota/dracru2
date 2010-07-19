de47dfd6ec4d33c2751cb083b83f67e3d96da08f
jemiam <jemiam@gmail.com>
2010-07-19 20:08:47 +0900

    tmp/skip.txtがあったら１回だけ飛ばす 闘技場の条件修正

--- send_army.rb
+++ send_army.rb
@@ -5,5 +5,10 @@ if File.exists?(File.join(TMP_PATH, 'stop.txt'))
   $logger.info("Raid was stopped. Remove tmp/stop.txt if you want to raid again.")
   exit
 end
+if File.exists?(File.join(TMP_PATH, 'skip.txt'))
+  File.delete(File.join(TMP_PATH, 'skip.txt'))
+  $logger.info("This raid was skipped.")
+  exit
+end
 dracru2 = Dracru2.new
 dracru2.send_army_if_possible
